2c9b74b15cc6b217125a9b4149effa8d9dab41ad
YKLee7238 <yklife8@naver.com>
2021-04-06 00:31:44 +0900

    고고

--- AlgorithmJAVA/Greedy.java
+++ AlgorithmJAVA/Greedy.java
@@ -11,6 +11,9 @@ public class Greedy {
 
         StrFlip("010100110");
 
+        System.out.println(Mujilive(new int[]{3,1,2}, 5));
+        
+
     }
 
     private static void LagerstNumber()
@@ -78,7 +81,7 @@ public class Greedy {
         System.out.println( String.format("실행 횟수는 : %d", cnt));
     }
 
-    private static void UntilOne2(int n, int k)
+    private static void UntilOneByAuthor(int n, int k)
     {
         /*Scanner sc = new Scanner(System.in);
 
@@ -108,9 +111,11 @@ public class Greedy {
 
     private static void StrFlip(String str)
     {
+        // 백준 1439
         int countZero =0, countOne =0;
         char p = str.charAt(0);
 
+        // 1로 바꾸는것과 0으로 바꾸는것 둘다 Count
         if(p == '1')
             countOne ++;
         else
@@ -118,6 +123,7 @@ public class Greedy {
         for(int i =1; i<str.length(); i++)
         {
             char c = str.charAt(i);
+            //앞 글자와 같은경우는 Count 하지 않음
             if( (p == c) ==false)
             {
                 if(c == '1')
@@ -130,6 +136,73 @@ public class Greedy {
         }
 
         System.out.println(Math.min(countZero, countOne));
-        
+    }
+
+    public static int Mujilive(int[] food_times, long k) {
+        // 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
+        long summary = 0;
+        for (int i = 0; i < food_times.length; i++) {
+            summary += food_times[i];
+        }
+        if (summary <= k) return -1;
+
+        // 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+        PriorityQueue<Food> pq = new PriorityQueue<>();
+        for (int i = 0; i < food_times.length; i++) {
+            // (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+            pq.offer(new Food(food_times[i], i + 1));
+        }
+
+        summary = 0; // 먹기 위해 사용한 시간
+        long previous = 0; // 직전에 다 먹은 음식 시간
+        long length = food_times.length; // 남은 음식의 개수
+
+        // summary + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
+        while (summary + ((pq.peek().getTime() - previous) * length) <= k) {
+            int now = pq.poll().getTime();
+            summary += (now - previous) * length;
+            length -= 1; // 다 먹은 음식 제외
+            previous = now; // 이전 음식 시간 재설정
+        }
+
+        // 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+        ArrayList<Food> result = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            result.add(pq.poll());
+        }
+        // 음식의 번호 기준으로 정렬
+        Collections.sort(result, new Comparator<Food>() { 
+            @Override
+            public int compare(Food a, Food b) {
+                return Integer.compare(a.getIndex(), b.getIndex());
+            }
+        });
+        return result.get((int) ((k - summary) % length)).getIndex();
+    }
+
+}
+
+class Food implements Comparable<Food> {
+
+    private int time;
+    private int index;
+
+    public Food(int time, int index) {
+        this.time = time;
+        this.index = index;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    // 시간이 짧은 것이 높은 우선순위를 가지도록 설정
+    @Override
+    public int compareTo(Food other) {
+        return Integer.compare(this.time, other.time);
     }
 }
