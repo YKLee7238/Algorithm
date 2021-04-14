796c0592c35171891f6decc4d0cdd5d8a7c6fb9b
YKLee7238 <yklife8@naver.com>
2021-04-14 23:45:50 +0900

    미로탈출

---
+++ AlgorithmJAVA/DFSBFS.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class DFSBFS {
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int m = 6;
+        int [][] map = new int[n][m];
+        //101010
+        //111111
+        //000001
+        //111111
+        //111111
+
+        Scanner sc = new Scanner(System.in);
+        for(int i =0; i<n; i ++){
+            String s = sc.nextLine();
+            for(int j=0; j<m; j++){
+                map[i][j] = Character.getNumericValue(s.charAt(j)); // char -> int 변환
+            }
+        }
+        sc.close();
+
+        System.out.println(BFS(0, 0, map));
+    }     
+
+    public static int BFS(int x, int y, int[][]map)
+    {
+        // 상 하 좌 우
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(x, y));
+
+        while(que.isEmpty() ==false){
+
+            Node node = que.poll();
+            x = node.getX();
+            y = node.getY();
+            // 현재 위치에서 4가지 방향으로의 위치 확인
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                // 미로 찾기 공간을 벗어난 경우 무시
+                if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length) continue;
+                // 벽인 경우 무시
+                if (map[nx][ny] == 0) continue;
+                // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+                if (map[nx][ny] == 1) {
+                    map[nx][ny] = map[x][y] + 1;
+                    que.offer(new Node(nx, ny));
+                } 
+            }
+        }
+        return map[map.length-1][map[0].length-1];
+    }    
+}
+class Node{
+    private int x, y;
+    Node(int x, int y){
+        this.x =x;
+        this.y=y;
+    }
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+}
